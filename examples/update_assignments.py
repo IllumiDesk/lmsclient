d12004721426a3c5c6760c476922b85d95fd425f
Greg Werner <werner.greg@gmail.com>
2021-05-06 18:52:14 -0400

    cleanup example
    Signed-off-by: Greg Werner <werner.greg@gmail.com>

--- examples/update_assignments.py
+++ examples/update_assignments.py
@@ -15,25 +15,21 @@ account_id = 1
 
 # Get a list of all course
 external_tools_by_course = canvasild.fetch_external_tools_by_course(course_id)
+
+# Get a list of all accounts
 external_tools_by_account = canvasild.fetch_external_tools_by_account(account_id)
 
+# Get external tools info
 for external_tool in external_tools_by_course:
     print(f'External tool for course {course_id} is {external_tool["id"]}')
 
+# Get the list of assignments for a course by course id
 assignments = canvasild.fetch_assignments(course_id)
 for assignment in assignments:
     print(f'Assignment id for course {course_id} is {assignment["id"]}')
     print(f'External Tool id (content_id) id for Assignment {assignment["id"]} is {assignment["external_tool_tag_attributes"]["content_id"]}')
 
-new_url = "https://flatiron-enterprise.illumidesk.com"
-new_content_id = 1305
-
-new_assignment = {}
-new_assignment['external_tool_tag_attributes'] = {}
-new_assignment['external_tool_tag_attributes']['url'] = new_url
-new_assignment['external_tool_tag_attributes']['content_id'] = new_content_id
-
-#payload={'assignment[external_tool_tag_attributes]': 'content_id: 1305',}
+# Create a dictionary that represents the external tool
 external_tool = {
     'url': 'https://flatiron-enterprise.illumidesk.com', 
     'new_tab': True, 
@@ -44,6 +40,7 @@ external_tool = {
     'custom': None,
 }
 
+# Update the assignment
 new_assignment_test = {
     'assignment[name]': 'Foo Bar',
     'assignment[external_tool_tag_attributes][url]': external_tool["url"],
@@ -52,6 +49,7 @@ new_assignment_test = {
     'assignment[peer_reviews]': True,
 }
 
+# Get the new assignment and print it's contents
 response_update_assignment = canvasild.update_assignment(course_id, 980, new_assignment_test)
 
 print(f'New assignment response {response_update_assignment}')
