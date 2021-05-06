6f5627dd04bcd0496df47e28ca35289b784f8a56
Greg Werner <werner.greg@gmail.com>
2021-05-06 13:47:29 -0400

    update canvas with external_tools methods
    Signed-off-by: Greg Werner <werner.greg@gmail.com>

--- lmsclient/canvas.py
+++ lmsclient/canvas.py
@@ -5,18 +5,16 @@ import requests
 
 from typing import List
 
+from .assignment import Assignment
+from .course import Course
+from .external_tool import ExternalTool
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-class LmsClientException(Exception):
-    """Custom exception class for all LMS clients
-    """
-    pass
-
-
-class CanvasClientException(LmsClientException):
+class CanvasClientException(Exception):
     """Canvas client exception
     """
     pass
@@ -69,19 +67,6 @@ class CanvasClient:
             headers=self.headers,)
 
         return response.json()
-
-    def fetch_course(self, course_id: str) -> json:
-        """Fetch course by id from a Canvas LMS instance
-        
-        Returns:
-          All course objects for a Canvas instance as a list of JSONs.
-        """
-        url = f'{self.api_url}/courses/{course_id}'
-        response = requests.get(
-            url,
-            headers=self.headers,)
-        
-        return response.json()
     
     def fetch_courses(self) -> List:
         """Fetch a list of courses from a Canvas LMS instance
@@ -96,7 +81,20 @@ class CanvasClient:
 
         return response.json()
 
-    def create_course(self, account_id: str, course:json) -> json:
+    def fetch_course(self, course_id: str) -> json:
+        """Fetch course by id from a Canvas LMS instance
+        
+        Returns:
+          All course objects for a Canvas instance as a list of JSONs.
+        """
+        url = f'{self.api_url}/courses/{course_id}'
+        response = requests.get(
+            url,
+            headers=self.headers,)
+        
+        return response.json()
+
+    def create_course(self, account_id: str, course: Course) -> json:
         """Create a course in a Canvas instance account
 
         Args:
@@ -193,70 +191,26 @@ class CanvasClient:
         response = requests.put(url, headers=self.headers, data=payload)
         return response.json()
 
-    def fetch_external_tool(self, course_id: str, external_tool_id: str) -> json:
-        """Fetch external tool by id
+    def create_course(self, *args) -> List:
+        result = 0
+        # Iterating over the Python args tuple
+        for x in args:
+            result += x
+        return result
 
-        Args:
-            external_tool_id (str): The external tool's id
-
+    def fetch_assignments(self, course_id: str)  -> List:
+        """Fetch a list of assignments for a course
+        
         Returns:
-            json: the external tool object as json
+            List: All assignment objects for a course as a list of JSONs.
         """
-        url = f'{self.api_url}/courses/{course_id}/external_tools/{external_tool_id}'
+        url = f'{self.api_url}/courses/{course_id}/assignments'
         response = requests.get(
             url,
             headers=self.headers,)
         
         return response.json()
 
-    def fetch_external_tools(self, course_id: str) -> List:
-        """Fetch the external tools for a course
-
-        Args:
-            course_id (str): The course id
-
-        Returns:
-            List: the external tools as a list
-        """
-        url = f'{self.api_url}/courses/{course_id}/external_tools'
-        response = requests.get(
-            url,
-            headers=self.headers,)
-        
-        return response.json()
-
-    def create_external_tool(self, course_id:str) -> json:
-        """Create an external tool for a course
-
-        Args:
-            course_id (str): the course id
-
-        Returns:
-            json: the external tool object as json
-        """
-        url = f'{self.api_url}/courses/{course_id}/external_tools'
-        response = requests.post(
-            url,
-            headers=self.headers,)
-        
-        return response.json()
-
-    def update_external_tool(self, course_id:str) -> json:
-        """Update an external tool for a course
-
-        Args:
-            course_id (str): the course id
-
-        Returns:
-            json: the external tool object as json
-        """
-        url = f'{self.api_url}/courses/{course_id}/external_tools'
-        response = requests.put(
-            url,
-            headers=self.headers,)
-        
-        return response.json()
-    
     def fetch_assignment(self, course_id: str, assignment_id: str) -> json:
         """Fetch assignment for a course by id
         
@@ -267,19 +221,6 @@ class CanvasClient:
           json: Assignment object as JSON
         """
         url = f'{self.api_url}/courses/{course_id}/assignments/{assignment_id}'
-        response = requests.get(
-            url,
-            headers=self.headers,)
-        
-        return response.json()
-    
-    def fetch_assignments(self, course_id: str)  -> List:
-        """Fetch a list of assignments for a course
-        
-        Returns:
-            List: All assignment objects for a course as a list of JSONs.
-        """
-        url = f'{self.api_url}/courses/{course_id}/assignments'
         response = requests.get(
             url,
             headers=self.headers,)
@@ -303,11 +244,140 @@ class CanvasClient:
         
         return response.json()
 
-    def update_assignment(self, course_id:str, assignment_id: str) -> json:
-        """Update an assignment's external tool attributes
+    def fetch_external_tools_by_course(self, course_id: str) -> json:
+        """Fetch a list of external tools by course id.
+
+        Args:
+            course_id (str): the course id
+
+        Returns:
+            json: the external tool object represented as json
+        """
+        url = f'{self.api_url}/courses/{course_id}/external_tools'
+        response = requests.get(url,
+            headers=self.headers)
+        
+        return response.json()
+
+
+    def fetch_external_tool_by_course(self, course_id:str, external_tool_id: str) -> json:
+        """Fetch an external tool by id.
+
+        Args:
+            course_id (str): the course id
+            external_tool_id (str): the external tool id
+
+        Returns:
+            json: the external tool object represented as json
+        """
+        url = f'{self.api_url}/courses/{course_id}/external_tools/{external_tool_id}'
+        response = requests.get(url,
+            headers=self.headers)
+        
+        return response.json()
+
+    def fetch_external_tools_by_account(self, account_id: str) -> json:
+        """Fetch a list of external tools by account id.
+
+        Args:
+            account_id (str): the account id
+
+        Returns:
+            json: the external tool object represented as json
+        """
+        url = f'{self.api_url}/accounts/{account_id}/external_tools'
+        response = requests.get(url,
+            headers=self.headers)
+        
+        return response.json()
+
+    def fetch_external_tool_by_account(self, account_id:str, external_tool_id: str) -> json:
+        """Fetch an external tool by id associated to an account.
+
+        Args:
+            account_id (str): the course id
+            external_tool_id (str): the external tool id
+
+        Returns:
+            json: the external tool object represented as json
+        """
+        url = f'{self.api_url}/accounts/{account_id}/external_tools/{external_tool_id}'
+        response = requests.get(url,
+            headers=self.headers)
+        
+        return response.json()
+
+    def create_external_tool_by_course(self, course_id:str) -> json:
+        """Create an external tool for a course
+
+        Args:
+            course_id (str): the course id
+
+        Returns:
+            json: the external tool object as json
+        """
+        url = f'{self.api_url}/courses/{course_id}/external_tools'
+        response = requests.post(
+            url,
+            headers=self.headers,)
+        
+        return response.json()
+
+    def update_external_tool_by_course(self, course_id:str) -> json:
+        """Update an external tool for a course
+
+        Args:
+            course_id (str): the course id
+
+        Returns:
+            json: the external tool object as json
+        """
+        url = f'{self.api_url}/courses/{course_id}/external_tools'
+        response = requests.put(
+            url,
+            headers=self.headers,)
+        
+        return response.json()
+
+    def create_external_tool_by_account(self, account_id:str) -> json:
+        """Create an external tool for a account
+
+        Args:
+            account_id (str): the account id
+
+        Returns:
+            json: the external tool object as json
+        """
+        url = f'{self.api_url}/accounts/{account_id}/external_tools'
+        response = requests.post(
+            url,
+            headers=self.headers,)
+        
+        return response.json()
+
+    def update_external_tool_by_account(self, account_id:str) -> json:
+        """Update an external tool for a account
+
+        Args:
+            account_id (str): the account id
+
+        Returns:
+            json: the external tool object as json
+        """
+        url = f'{self.api_url}/accounts/{account_id}/external_tools'
+        response = requests.put(
+            url,
+            headers=self.headers,)
+        
+        return response.json()
+
+    def update_assignment(self, course_id:str, assignment_id: str, assignment: Assignment) -> json:
+        """Update an assignment in a course
         
         Args:
+            course_id (str): the course that has the assignment
             assignment_id (str): the assignment id to update
+            assignment (Assignment): the updated assignment
         
         Returns:
             json: Updated assignment object as JSON
@@ -317,7 +387,7 @@ class CanvasClient:
         response = requests.put(url,
             headers=self.headers,
             json=assignment.json())
-                
+
         payload = {
             assignment['name']: '',
             assignment['position']: '',
